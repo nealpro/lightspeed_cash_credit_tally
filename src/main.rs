eb23a63bce5d44d35fcb2bdafab7a3bb4881af67
nealpro <146895084+nealpro@users.noreply.github.com>
2023-12-17 11:12:20 -0500

    Add .gitignore, Cargo.toml, and main.rs files

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use reqwest;
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Deserialize, Debug)]
+struct ApiResponse {
+    CCCharge: Vec<Charge>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Charge {
+    amount: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let account_id = "your_account_id"; // Replace with your account ID
+    let access_token = "your_access_token"; // Replace with your access token
+
+    let client = reqwest::Client::new();
+    let res = client
+        .get(format!(
+            "https://api.lightspeedapp.com/API/V3/Account/{}/CCCharge.json",
+            account_id
+        ))
+        .bearer_auth(access_token)
+        .send()
+        .await?
+        .json::<ApiResponse>()
+        .await?;
+
+    for charge in res.CCCharge {
+        println!("Amount: {}", charge.amount);
+    }
+
+    Ok(())
+}
