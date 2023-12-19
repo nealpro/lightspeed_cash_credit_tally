b673647c12f4d666128af510bf03b78ace3cb03f
nealpro <146895084+nealpro@users.noreply.github.com>
2023-12-19 09:40:24 -0500

    Add get_access_token function to Secrets struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+mod secrets;
+
 use reqwest;
 use serde::Deserialize;
 use std::error::Error;
 
 #[derive(Deserialize, Debug)]
 struct ApiResponse {
-    CCCharge: Vec<Charge>,
+    cccharge: Vec<Charge>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -14,7 +16,7 @@ struct Charge {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let account_id = "your_account_id"; // Replace with your account ID
+    let account_id = "your_account_id";
     let access_token = "your_access_token"; // Replace with your access token
 
     let client = reqwest::Client::new();
@@ -29,7 +31,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .json::<ApiResponse>()
         .await?;
 
-    for charge in res.CCCharge {
+    for charge in res.cccharge {
         println!("Amount: {}", charge.amount);
     }
 
